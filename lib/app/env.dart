8d189c2b4f52cddce4fbb0dbcb34cd1261c4545d
Oli Goldwait <youremail@example.com>
2025-10-12 09:26:52 +0100

    revert: restore preprocess page from .bak and remove env.dart

--- lib/app/env.dart
+++
@@ -1,7 +0,0 @@
-﻿class Env {
-  static const String apiBase =
-      String.fromEnvironment('API_BASE', defaultValue: 'http://127.0.0.1:8000');
-
-  static const String month =
-      String.fromEnvironment('BID_MONTH', defaultValue: '2025-11');
-}
--- lib/features/preprocess/preprocess_page.dart
+++ lib/features/preprocess/preprocess_page.dart
@@ -1,7 +1,13 @@
-﻿ param($m)
-      $imports = $m.Value
-      $imports + "import 'package:nextbid/app/env.dart';`r`n"
-    
+import "package:flutter/material.dart";
+import "../../shared/services/app_state.dart";
+import "../../shared/services/api_client.dart";
+import "../../shared/widgets/credit_range_selector.dart";
+import "../../shared/widgets/leave_slide_visualizer.dart";
+import "../../shared/widgets/logout_leading.dart";
+import "../../shared/widgets/reserve_calendar.dart";
+import 'package:nextbid_demo/jss/jss_commands.dart';
+import 'package:nextbid_demo/utils/exporter.dart';
+
 class PreProcessPage extends StatefulWidget {
   const PreProcessPage({super.key});
   @override
@@ -498,4 +504,3 @@ class _SummaryCard extends StatelessWidget {
     ),
   );
 }
-
