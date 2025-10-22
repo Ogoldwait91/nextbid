e031a8c4f1066c47d301d405d5d6d1c1acfe0cf4
Oli Goldwait <youremail@example.com>
2025-10-22 08:35:27 +0100

    feat(settings): add SettingsService (protectBank, lastCredit) using shared_preferences

---
+++ lib/services/settings_service.dart
@@ -0,0 +1,26 @@
+﻿import "package:shared_preferences/shared_preferences.dart";
+
+class SettingsService {
+  static const _kProtectBank = "protectBank";
+  static const _kLastCredit  = "lastCredit";
+
+  Future<bool> getProtectBank() async {
+    final p = await SharedPreferences.getInstance();
+    return p.getBool(_kProtectBank) ?? false;
+  }
+
+  Future<void> setProtectBank(bool value) async {
+    final p = await SharedPreferences.getInstance();
+    await p.setBool(_kProtectBank, value);
+  }
+
+  Future<int?> getLastCredit() async {
+    final p = await SharedPreferences.getInstance();
+    return p.getInt(_kLastCredit);
+  }
+
+  Future<void> setLastCredit(int value) async {
+    final p = await SharedPreferences.getInstance();
+    await p.setInt(_kLastCredit, value);
+  }
+}
