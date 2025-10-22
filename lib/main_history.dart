141c2c111c5d181d710b56b735cabd6d3fb27df8
Oli Goldwait <youremail@example.com>
2025-10-22 08:03:46 +0100

    chore(history): silence key parameter lint in main_history entrypoint

--- lib/main_history.dart
+++ lib/main_history.dart
@@ -1,4 +1,4 @@
-﻿import "package:flutter/material.dart";
+import "package:flutter/material.dart";
 import "features/export_history/export_history_page.dart";
 
 void main() {
@@ -6,15 +6,16 @@ void main() {
 }
 
 class _HistoryApp extends StatelessWidget {
-  const _HistoryApp({super.key});
+  const _HistoryApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "NextBid – Export History",
+      title: "NextBid � Export History",
       debugShowCheckedModeBanner: false,
       theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.indigo),
       home: const ExportHistoryPage(),
     );
   }
 }
+
